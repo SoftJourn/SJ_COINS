3cce5e5ce336c08c9d6970152b7deaaaeaf6ccf7
volodymyr <vromanchuk@softjourn.com>
2016-09-02 17:51:59 +0300

    Getting property values. Set up root user accounts .

--- src/test/java/com/softjourn/coin/server/service/CoinServiceTransactionsTestContextConfiguration.java
+++ src/test/java/com/softjourn/coin/server/service/CoinServiceTransactionsTestContextConfiguration.java
@@ -1,26 +1,51 @@
 package com.softjourn.coin.server.service;
 
 
+
 import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
+
 @Configuration
+
 @ComponentScan(basePackages = "com.softjourn.coin.server.service")
 @EnableJpaRepositories(basePackages = "com.softjourn.coin.server.repository", entityManagerFactoryRef="entityManagerFactory")
 @EntityScan(basePackages = "com.softjourn.coin.server.entity")
 @EnableTransactionManagement
+@PropertySource(value= {"classpath:application-test.properties"})
 public class CoinServiceTransactionsTestContextConfiguration {
+
+    //To resolve ${} in @Value
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Value(value="${root}")
+    private String rootUser;
+
+    @Bean
+    public String rootUser(){
+        System.out.println(rootUser);
+        return rootUser;
+    }
 
     @Bean
     public EmbeddedDatabase dataSource() {
