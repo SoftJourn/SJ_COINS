83e753a09c8f8c89c512790aa7996b3b10fcb9de
ovovchuk <ovovchuk@softjourn.com>
2016-10-26 17:25:01 +0300

    Removed local ssl configuration from properties.

--- src/test/sh/addAmount.sh
+++ src/test/sh/addAmount.sh
@@ -18,7 +18,7 @@ tokens=(`curl --silent -i -k -G -X POST \
   -d "username="${yourLDAPid} \
   -d "password="${yourLDAPpassword} \
   -d "grant_type=password" \
-  https://localhost:8111/oauth/token \
+  http://localhost:8111/oauth/token \
   | grep -Po "((?<=access_token\":\")[^\"]+)|((?<=refresh_token\":\")[^\"]+)"`)
 
 echo "ACCESS_TOKEN: "${tokens[0]}
@@ -27,7 +27,7 @@ echo "REFRESH_TOKEN: "${tokens[1]}
 declare data='{"comment": "Pass some money to '${DESTINATION}'", "amount":'${AMOUNT}"}"
 
 curl --silent -i -k -X POST \
-  https://localhost:8080/api/v1/add/${DESTINATION} \
+  http://localhost:8080/api/v1/add/${DESTINATION} \
   -d "${data}" \
   -H "Authorization: Bearer "${tokens[0]} \
   -H "Content-Type: application/json"
--- src/test/sh/getAmount.sh
+++ src/test/sh/getAmount.sh
@@ -15,13 +15,13 @@ tokens=(`curl --silent -i -k -G -X POST \
   -d "username="${yourLDAPid} \
   -d "password="${yourLDAPpassword} \
   -d "grant_type=password" \
-  https://localhost:8111/oauth/token \
+  http://localhost:8111/oauth/token \
   | grep -Po "((?<=access_token\":\")[^\"]+)|((?<=refresh_token\":\")[^\"]+)"`)
 
 echo "ACCESS_TOKEN: "${tokens[0]}
 echo "REFRESH_TOKEN: "${tokens[1]}
 
 curl --silent -i -k -X  GET \
-  https://localhost:8080/api/v1/amount \
+  http://localhost:8080/api/v1/amount \
   -H "Authorization: Bearer "${tokens[0]} \
 
