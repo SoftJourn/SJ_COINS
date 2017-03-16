9c23beebd3f72cab3ad8fcf014185d8b11614fdd
vdanyliuk <vdanyliuk@softjourn.com>
2017-03-16 15:34:31 +0200

    Define eris version in properties

--- src/main/java/com/softjourn/coin/server/config/BlockChainConfig.java
+++ src/main/java/com/softjourn/coin/server/config/BlockChainConfig.java
@@ -12,6 +12,8 @@ import com.softjourn.eris.transaction.parser.ErisParser;
 import com.softjourn.eris.transaction.parser.ErisParserService;
 import com.softjourn.eris.transaction.parser.v11.Eris11CallTransactionParser;
 import com.softjourn.eris.transaction.parser.v11.Eris11ParserService;
+import com.softjourn.eris.transaction.parser.v12.Eris12CallTransactionParser;
+import com.softjourn.eris.transaction.parser.v12.Eris12ParserService;
 import com.softjourn.eris.transaction.pojo.ErisCallTransaction;
 import com.softjourn.eris.transaction.pojo.ErisTransaction;
 import com.softjourn.eris.transaction.pojo.ErisTransactionType;
@@ -30,14 +32,40 @@ import java.util.function.Function;
 @Slf4j
 public class BlockChainConfig {
 
+    private static final Map<String, ErisParser[]> SUPPORTED_PARSERS = new HashMap<String, ErisParser[]>() {{
+        put("0.11", new ErisParser[]{new Eris11CallTransactionParser()});
+        put("0.12", new ErisParser[]{new Eris12CallTransactionParser()});
+    }};
+
+    private static final Map<String, Function<ErisParser[], ErisParserService>> SUPPORTED_PARSER_SERVICES = new HashMap<String, Function<ErisParser[], ErisParserService>>() {{
+        put("0.11", Eris11ParserService::new);
+        put("0.12", Eris12ParserService::new);
+    }};
+
+    @Value("${eris.chain.version}")
+    private String erisChainVersion;
+
     @Bean
-    public ErisParser parser() {
-        return new Eris11CallTransactionParser();
+    public ErisParser[] parsers() {
+        ErisParser[] parsers = SUPPORTED_PARSERS.get(getErisChainVersion());
+        if (parsers == null) throw new IllegalStateException("Can't find any Eris parsers for version " + getErisChainVersion());
+        return parsers;
     }
 
     @Bean
     public ErisParserService parserService(ErisParser... parsers) {
-        return new Eris11ParserService(parsers);
+        Function<ErisParser[], ErisParserService> parserServiceFunction = SUPPORTED_PARSER_SERVICES.get(getErisChainVersion());
+        if (parserServiceFunction == null) throw new IllegalStateException("Can't find any Eris parser service for version " + getErisChainVersion());
+        return parserServiceFunction.apply(parsers);
+    }
+
+    private String getErisChainVersion() {
+        if (erisChainVersion == null) {
+            throw new IllegalStateException("Eris version not specified. Add \"eris.chain.version\" property. " +
+                    "Currently supported: " + SUPPORTED_PARSERS.keySet());
+        } else {
+            return erisChainVersion;
+        }
     }
 
     @Bean
@@ -52,22 +80,22 @@ public class BlockChainConfig {
     }
 
     private Consumer<? extends ErisUndefinedTransaction> getUndefinedTxConsumer() {
-        return transaction -> log.info("ERIS UNDEFINED TX: "+transaction.toString());
+        return transaction -> log.info("ERIS UNDEFINED TX: " + transaction.toString());
     }
 
-    private Consumer<? extends ErisCallTransaction> getCallTxConsumer(ErisTransactionHistoryService historyService){
+    private Consumer<? extends ErisCallTransaction> getCallTxConsumer(ErisTransactionHistoryService historyService) {
         return transaction -> historyService
-            .storeTransaction(mapTransaction(transaction));
+                .storeTransaction(mapTransaction(transaction));
 
     }
 
-    private Function<String, String> getAbiFromContractAddress(ContractService contractService){
+    private Function<String, String> getAbiFromContractAddress(ContractService contractService) {
         return address -> contractService
-            .getContractsByAddress(address)
-            .getAbi();
+                .getContractsByAddress(address)
+                .getAbi();
     }
 
-    private TransactionStoring mapTransaction(ErisCallTransaction transaction){
+    private TransactionStoring mapTransaction(ErisCallTransaction transaction) {
         ErisTransactionDAO transactionDAO = new ErisTransactionDAO(transaction);
         return new TransactionStoring(transactionDAO);
     }
